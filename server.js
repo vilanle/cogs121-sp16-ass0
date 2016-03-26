7f81d856caa5f168242ae2516e0acc3a669cd5fe
Brian Soe <bsoe@ucsd.edu>
2016-03-25 22:53:22 -0500

    added procfile for heroku deployment

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ var parser = {
 
 // // Database Connection
 // var db = mongoose.connection;
-// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/cogs121');
+// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/cogs121');
 // db.on('error', console.error.bind(console, 'Mongo DB Connection Error:'));
 // db.once('open', function(callback) {
-//     console.log("Database connected succesfully.");
+//     console.log("Database connected successfully.");
 // });
 
 // Middleware
